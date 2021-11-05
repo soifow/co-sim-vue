791ac13167aab3fe032d9f42b1fc04084eef18d8
soifow <soifow@users.noreply.github.com>
2021-11-05 14:57:04 +0800

    update 工程配置
    1.添加了基础过滤器（时间、日期格式化etc）
    2.添加了路由表格式说明md文件
    3.新增了部分字段定义

--- src/main.js
+++ src/main.js
@@ -15,7 +15,29 @@ import 'ant-design-vue/dist/antd.less';  // or 'ant-design-vue/dist/antd.less'
 // 设备弹窗使用的的css变量定义
 import '@assets/modal.css'
 
+// import '@/permission' // 权限控制（参考swan-hostings-vue），工程当前未引入
+import '@/utils/filter'   // 时间等基础格式过滤器
+
+import {
+  ACCESS_TOKEN,
+  DEFAULT_COLOR,
+  DEFAULT_THEME,
+  DEFAULT_LAYOUT_MODE,
+  DEFAULT_COLOR_WEAK,
+  SIDEBAR_TYPE,
+  DEFAULT_FIXED_HEADER,
+  DEFAULT_FIXED_HEADER_HIDDEN,
+  DEFAULT_FIXED_SIDEMENU,
+  DEFAULT_CONTENT_WIDTH_TYPE,
+  DEFAULT_MULTI_PAGE,
+  PROJECTINFO
+} from "@/store/mutation-types"
+import config from '@/defaultSettings'
+
 Vue.config.productionTip = false
+Vue.use(Storage, config.storageOptions)
+Vue.use(Antd)
+Vue.use(VueAxios, router)
 
 new Vue({
   router,
